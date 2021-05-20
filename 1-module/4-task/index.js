0427956ede6dceb13a955977f8d7619650ffca2e
Iana <sianalucky@gmail.com>
2021-05-20 12:22:24 +0300

    task5

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,13 +1,8 @@
 function checkSpam(str) {
-<<<<<<< HEAD
+
   let tempStr = str.toLowerCase(); 
   if (tempStr.includes("1xbet") || tempStr.includes("xxx")) {
 	  return true;
   }
   return false;
-=======
-  let strLowerCase = str.toLowerCase();
-
-  return strLowerCase.includes('1xbet') || strLowerCase.includes('xxx');
->>>>>>> 55ec613b5f84ec168c732fb57949db6544792d2c
 }
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,12 @@
-function truncate(str, maxlength) {
-  // ваш код...
-}
+
+  function truncate (str, max){
+    let point = "…";
+    if (str.length > max){
+      return str.slice(0, (max - 1)) + point;
+      // str.substr(0, (max - 1)) + '…';
+    }
+    else {
+      return str;
+    }
+  }
+  truncate ('Вот, что мне хотелось бы сказать на эту тему:', 20)
