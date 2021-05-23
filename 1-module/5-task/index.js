f02f5f28d192dcb0ee9cecd8228b0cc658f0cc23
Iana <sianalucky@gmail.com>
2021-05-23 13:27:46 +0300

    fix task 5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -3,10 +3,8 @@
     let point = "…";
     if (str.length > max){
       return str.slice(0, (max - 1)) + point;
-      // str.substr(0, (max - 1)) + '…';
     }
-    else {
-      return str;
-    }
+   return str;
+    
   }
   truncate ('Вот, что мне хотелось бы сказать на эту тему:', 20)
